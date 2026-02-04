f55702d54f94fa4a656d20fce54db760465bc9ab
dhruv-itwala <dhruvitwala@gmail.com>
2026-02-04 11:55:06 +0530

    feat: update CORS configuration to allow PATCH method

--- Backend/config/cors.config.js
+++ Backend/config/cors.config.js
@@ -4,7 +4,7 @@ const corsOptions = {
     return callback(null, origin); // echo origin
   },
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
 
