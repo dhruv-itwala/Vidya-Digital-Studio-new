85eb76a7ea377d872d6c443141940689cf613516
dhruv-itwala <dhruvitwala@gmail.com>
2026-02-08 23:42:39 +0530

    feat: import useEffect hook in BusinessCard component

--- QrServices/src/components/BusinessCard/BusinessCard.jsx
+++ QrServices/src/components/BusinessCard/BusinessCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./BusinessCard.module.css";
 import { Images } from "../../assets/Data/images";
 import { Icon } from "@iconify/react";
